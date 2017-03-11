7beac99007186905686226efdef3d604bcedb099
shalini saxena <shalini.saxena@affiliate.declara.com>
2017-03-11 12:56:16 -0800

    adding decompress function to Run len encoding

--- src/ArraysAdHoc/RunLenEncoding.java
+++ src/ArraysAdHoc/RunLenEncoding.java
@@ -6,7 +6,7 @@ package ArraysAdHoc;
 public class RunLenEncoding {
 
     static String RLE(String strinput) {
-        if(strinput == null || strinput.isEmpty()){
+        if (strinput == null || strinput.isEmpty()) {
             return null;
         }
 
@@ -15,15 +15,15 @@ public class RunLenEncoding {
         StringBuffer stringBuffer = new StringBuffer();
         int freq = 0;
 
-        for(int i = 0; i < input.length; i++){
+        for (int i = 0; i < input.length; i++) {
 
             freq = 1;
 
-            while( i + 1 < input.length && input[i] == input [i+1]){
+            while (i + 1 < input.length && input[i] == input[i + 1]) {
                 freq++;
                 i++;
             }
-            if(freq > 1) {
+            if (freq > 1) {
                 stringBuffer.append(freq).append(input[i]);
             } else {
                 stringBuffer.append(input[i]);
@@ -34,11 +34,53 @@ public class RunLenEncoding {
         return stringBuffer.toString();
     }
 
-    public static void main(String a []){
-        String val = "BABABA";
+    public static String decompress(String value) {
+
+        char[] input = value.toCharArray();
+        StringBuffer stbuff = new StringBuffer();
+
+        for (int i = 0; i < input.length; i++) {
+            StringBuffer frequency = new StringBuffer();
+            char ch = input[i];
+
+            if(!isANumber(ch)){
+                stbuff.append(ch);
+            }
+
+            while (input[i] >= '0' && input[i] <= '9') {
+                frequency.append(ch);
+                i++;
+            }
+
+            int freqCount = frequency.length() > 0 ? Integer.parseInt(frequency.toString()) : 0 ;
+            ch = input[i];
+            while (freqCount > 0) {
+               stbuff.append(ch);
+               freqCount--;
+            }
+
+        }
+
+        return stbuff.toString();
+    }
+
+    private static boolean isANumber(char ch) {
+       if(ch >= '0' && ch <= '9'){
+           return true;
+       }
+
+       return false;
+    }
+
+    public static void main(String a[]) {
+        String val = "BCAAAB";
 
         String out = RLE(val);
 
-        System.out.print(out);
+        System.out.println(out);
+
+        System.out.print(decompress(out));
+
+
     }
 }
--- src/Graphs/CountIslands.java
+++ src/Graphs/CountIslands.java
@@ -6,121 +6,120 @@ import java.util.List;
 /**
  * Given a boolean 2D matrix, find the number of islands. A group of connected 1s forms an island.
  * For example, the below matrix contains 5 islands
-
-Input : mat[][] = {{1, 1, 0, 0, 0},
-                   {0, 1, 0, 0, 1},
-                   {1, 0, 0, 1, 1},
-                   {0, 0, 0, 0, 0},
-                   {1, 0, 1, 0, 1} 
-Output : 5
- * @author shalinishah
+ * <p>
+ * Input : mat[][] = {{1, 1, 0, 0, 0},
+ * {0, 1, 0, 0, 1},
+ * {1, 0, 0, 1, 1},
+ * {0, 0, 0, 0, 0},
+ * {1, 0, 1, 0, 1}
+ * Output : 5
  *
+ * @author shalinishah
  */
 public class CountIslands {
 
-	static class Cell {
-		private int x, y;
+    static class Cell {
+        private int x, y;
 
-		public Cell(int a, int b) {
-			this.x = a;
-			this.y = b;
-		}
-	}
+        public Cell(int a, int b) {
+            this.x = a;
+            this.y = b;
+        }
+    }
 
-	private static int ROW;
-	private static int COL;
-	
-	public static List<Cell> getNeighbors(int[][] islandMatrix, int x, int y) {
+    private static int ROW;
+    private static int COL;
 
-		List<Cell> neighbor = new ArrayList<>();
+    public static List<Cell> getNeighbors(int[][] islandMatrix, int x, int y) {
 
-		int xRange[] = { -1, -1, -1, 0, 1, 1, 1, 0 };
-		int yRange[] = { -1, 0, 1, 1, 1, 0, -1, -1 };
+        List<Cell> neighbor = new ArrayList<>();
 
-		for (int i = 0; i < xRange.length; i++) {
+        int xRange[] = {-1, -1, -1, 0, 1, 1, 1, 0};
+        int yRange[] = {-1, 0, 1, 1, 1, 0, -1, -1};
 
-			for (int j = 0; j < yRange.length; j++) {
-				int x1 = x + xRange[i];
-				int y1 = y + yRange[j];
+        for (int i = 0; i < xRange.length; i++) {
 
-				if (isValidCoord(x1, y1)) {
-					if (islandMatrix[x1][y1] == 1) {
-						neighbor.add(new Cell(x1, y1));
-					}
-				}
-			}
-		}
+            for (int j = 0; j < yRange.length; j++) {
+                int x1 = x + xRange[i];
+                int y1 = y + yRange[j];
 
-		return neighbor;
+                if (isValidCoord(x1, y1)) {
+                    if (islandMatrix[x1][y1] == 1) {
+                        neighbor.add(new Cell(x1, y1));
+                    }
+                }
+            }
+        }
 
-	}
+        return neighbor;
 
-	private static boolean isValidCoord(int x1, int y1) {
+    }
 
-		if (x1 < 0 || x1 >= ROW || y1 < 0 || y1 >= COL) {
-			return false;
-		}
+    private static boolean isValidCoord(int x1, int y1) {
 
-		return true;
-	}
+        if (x1 < 0 || x1 >= ROW || y1 < 0 || y1 >= COL) {
+            return false;
+        }
 
-	public static int countIslands(int[][] matrix) {
-		ROW = matrix.length;
-		COL = matrix[0].length;
-		
-		int count = 0;
-		boolean[][] visited = new boolean[ROW][COL];
+        return true;
+    }
 
-		for (int i = 0; i < ROW; i++) {
-			for (int j = 0; j < COL; j++) {
+    public static int countIslands(int[][] matrix) {
+        ROW = matrix.length;
+        COL = matrix[0].length;
 
-				if (matrix[i][j] == 1 && !visited[i][j]) {
-					exhaustComponent(matrix, i, j, visited);
-					count++;
-				}
-			}
-		}
-		
-		return count;
-	}
+        int count = 0;
+        boolean[][] visited = new boolean[ROW][COL];
 
-	//DFS to exhaust all neighbors
-	private static void exhaustComponent(int[][] matrix, int x, int y, boolean[][] visited) {
-		
-		ArrayList<Cell> neighbors  = (ArrayList<Cell>) getNeighbors(matrix, x, y);
-		
-		visited[x][y]= true;
-		
-		for(Cell c : neighbors){
-			int x1 = c.x;
-			int y1 = c.y;
-			if(!visited[x1][y1]){
-				exhaustComponent(matrix, x1, y1, visited);
-			}
-		}
-		
-	}
-	
-	public static void main(String args []){
-	 int islandMatrix[][] = {{1, 1, 0, 0, 0},
-             {0, 1, 0, 0, 1},
-             {1, 0, 0, 1, 1},
-             {0, 0, 0, 0, 0},
-             {1, 0, 1, 0, 1}};
-	 
-	 int count = countIslands(islandMatrix);
-	 
-	 System.out.println(count);
-	 
-	int matrix[][] = {{1, 1},
-             {0, 1},
-             {1, 0},
-             {0, 0},
-             {1, 0}};
-	 
-	  count = countIslands(matrix);
-	 
-	 System.out.println(count);
-	 }
+        for (int i = 0; i < ROW; i++) {
+            for (int j = 0; j < COL; j++) {
+
+                if (matrix[i][j] == 1 && !visited[i][j]) {
+                    exhaustComponent(matrix, i, j, visited);
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    //DFS to exhaust all neighbors
+    private static void exhaustComponent(int[][] matrix, int x, int y, boolean[][] visited) {
+
+        ArrayList<Cell> neighbors = (ArrayList<Cell>) getNeighbors(matrix, x, y);
+
+        visited[x][y] = true;
+
+        for (Cell c : neighbors) {
+            int x1 = c.x;
+            int y1 = c.y;
+            if (!visited[x1][y1]) {
+                exhaustComponent(matrix, x1, y1, visited);
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        int islandMatrix[][] = {{1, 1, 0, 0, 0},
+                {0, 1, 0, 0, 1},
+                {1, 0, 0, 1, 1},
+                {0, 0, 0, 0, 0},
+                {1, 0, 1, 0, 1}};
+
+        int count = countIslands(islandMatrix);
+
+        System.out.println(count);
+
+        int matrix[][] = {{1, 1},
+                {0, 1},
+                {1, 0},
+                {0, 0},
+                {1, 0}};
+
+        count = countIslands(matrix);
+
+        System.out.println(count);
+    }
 
 }
